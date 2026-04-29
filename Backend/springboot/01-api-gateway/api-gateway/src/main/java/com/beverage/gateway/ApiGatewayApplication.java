c291ca68394bb3dda9c171d5b8def12ccfcee183
TNJKL <sinhngay21042004@gmail.com>
2026-04-29 16:34:16 +0700

    init: project structure

---
+++ Backend/springboot/01-api-gateway/api-gateway/src/main/java/com/beverage/gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.beverage.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ Backend/springboot/04-order-service/order-service/src/main/java/com/beverage/order/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.beverage.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
