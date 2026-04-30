4221330918c35a1d2808ab5cf7a826ac3772b04f
TNJKL <sinhngay21042004@gmail.com>
2026-05-01 00:35:45 +0700

    set up thanh cong gateway

--- Backend/Springboot/auth-service/src/main/java/com/beverage/auth/AuthServiceApplication.java
+++ Backend/Springboot/auth-service/src/main/java/com/beverage/auth/AuthServiceApplication.java
@@ -3,7 +3,11 @@ package com.beverage.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
--- Backend/Springboot/eureka-server/src/main/java/com/beverage/eureka/EurekaServerApplication.java
+++ Backend/Springboot/eureka-server/src/main/java/com/beverage/eureka/EurekaServerApplication.java
@@ -3,7 +3,10 @@ package com.beverage.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
